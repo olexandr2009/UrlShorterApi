4b59357e1014e40b673dd522be6d82ade360234f
olexandr2009 <hristevich.ua@gmail.com>
2024-01-12 15:14:46 +0200

    Initial commit

---
+++ src/main/java/com/url/shorter/UrlShorterApiApplication.java
@@ -0,0 +1,13 @@
+package com.url.shorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShorterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/url/shorter/UrlShorterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.shorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
