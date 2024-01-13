546266270d8847371b340c05b2c8f3a05b4e04e6
olexandr2009 <133679311+olexandr2009@users.noreply.github.com>
2024-01-13 17:14:54 +0200

    Revert "Khrystevich auth"

--- src/main/java/com/url/shorter/features/link/services/ShortLinkGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.url.shorter.features.link.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ShortLinkGenerator {
-}
--- src/test/java/com/url/shorter/TestUrlShorterApiApplication.java
+++ src/test/java/com/url/shorter/TestUrlShorterApiApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
