10dacf34aaa875e121798a61ef74e9997415bc1c
olexandr2009 <hristevich.ua@gmail.com>
2024-01-13 12:31:43 +0200

    auth added

--- src/main/java/com/url/shorter/features/auth/web/AuthController.java
+++ src/main/java/com/url/shorter/features/auth/web/AuthController.java
@@ -12,7 +12,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
