7a8e36275d027664f21e172adbfe97676a049063
Leonid <leonidg34@gmail.com>
2024-01-14 13:01:39 +0100

    db migration link, entity link

--- src/main/java/com/url/shorter/features/link/entities/LinkEntity.java
+++ src/main/java/com/url/shorter/features/link/entities/LinkEntity.java
@@ -1,4 +1,50 @@
 package com.url.shorter.features.link.entities;
 
+import com.url.shorter.features.user.entities.UserEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.security.Timestamp;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "l_link_s_link")
 public class LinkEntity {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private String id = UUID.randomUUID().toString();
+
+        @Column(name = "long_link", nullable = false)
+        private String longLink;
+
+        @Column(name = "short_link", nullable = false)
+        private String shortLink;
+
+        @Column(name = "id_user", nullable = false)
+        private int userId;
+
+        @Column(name = "creation_date", nullable = false)
+        private Timestamp creationDate;
+
+        @Column(name = "expiration_date", nullable = false)
+        private Timestamp expirationDate;
+
+        @Column(name = "clicks", nullable = false)
+        private int clicks;
+
+        @ManyToOne
+        @JoinColumn(name = "id_user", referencedColumnName = "id", insertable = false, updatable = false)
+        private UserEntity user;
+
+        public LinkEntity(String longLink, String shortLink, int userId, Timestamp creationDate, Timestamp expirationDate, int clicks, UserEntity user) {
+                this.longLink = longLink;
+                this.shortLink = shortLink;
+                this.userId = userId;
+                this.creationDate = creationDate;
+                this.expirationDate = expirationDate;
+                this.clicks = clicks;
+                this.user = user;
+        }
 }
