74ba9852041a3c9c63da01414a1899d2275f8b94
Vitalii Shliakhetko <wwvitaliy@gmail.com>
2024-01-14 11:08:22 +0200

    LinkEntity, LinkDta, LinkRepository created

--- src/main/java/com/url/shorter/features/link/entities/LinkEntity.java
+++ src/main/java/com/url/shorter/features/link/entities/LinkEntity.java
@@ -1,4 +1,53 @@
 package com.url.shorter.features.link.entities;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Link entity
+ */
+@Entity
+@Table(name = "links")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class LinkEntity {
+    @Id
+    @Column(name = "id")
+    UUID id;
+
+    @Column(name = "short_url", length = 25)
+    @NotNull
+    String shortUrl;
+
+    @Column(name = "origin_url", length = 250)
+    @NotNull
+    String originUrl;
+
+    @Column(name = "created_date")
+    @NotNull
+    LocalDateTime createdDate;
+
+    @Column(name = "open_count")
+    @NotNull
+    Integer openCount;
+
+    @Column(name = "expiration_date")
+    @NotNull
+    LocalDateTime expirationDate;
+
+    @Column(name = "user_id")
+    @NotNull
+    UUID userId;
+
 }
--- src/main/java/com/url/shorter/features/link/repositories/LinkRepository.java
+++ src/main/java/com/url/shorter/features/link/repositories/LinkRepository.java
@@ -1,7 +1,12 @@
 package com.url.shorter.features.link.repositories;
 
+import com.url.shorter.features.link.entities.LinkEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Spring repository for LinkEntity
+ */
 @Repository
-public interface LinkRepository {
+public interface LinkRepository extends JpaRepository<LinkEntity, String> {
 }
