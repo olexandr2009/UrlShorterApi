0a0e37e465e9f7dfb5b242b89568dcf4a19e974d
Serhii <serhii.mask@gmail.com>
2024-01-17 11:08:45 +0200

    feat: add two method in LinkService

--- src/main/java/com/url/shorter/features/link/services/LinkService.java
+++ src/main/java/com/url/shorter/features/link/services/LinkService.java
@@ -1,4 +1,13 @@
 package com.url.shorter.features.link.services;
 
+import com.url.shorter.features.link.entities.LinkEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public interface LinkService {
+    Optional<LinkEntity> findByShortLink(String shortLink);
+
+    void deleteByShortLink(String shortLink);
 }
