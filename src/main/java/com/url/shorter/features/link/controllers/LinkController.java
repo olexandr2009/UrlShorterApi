3fb3f8b2499f9fccd95f1d90a9642bb50deacba8
Serhii <serhii.mask@gmail.com>
2024-01-17 11:10:27 +0200

    feat: add two method in LinkController

--- src/main/java/com/url/shorter/features/link/controllers/LinkController.java
+++ src/main/java/com/url/shorter/features/link/controllers/LinkController.java
@@ -1,14 +1,15 @@
 package com.url.shorter.features.link.controllers;
 
+import com.url.shorter.features.link.entities.LinkEntity;
 import com.url.shorter.features.link.services.LinkService;
-import jakarta.validation.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -18,16 +19,15 @@ public class LinkController {
     @Autowired
     private LinkService linkService;
 
-    @RequestMapping(value = "/{shortLink}", method = {RequestMethod.GET})
-    public String findLinkByShortLink(@NotEmpty @RequestParam(value="shortLink") String shortLink) {
-        return linkService.findByShortLink();
+    @GetMapping("/{shortLink}")
+    public ResponseEntity<LinkEntity> findLinkByShortLink(@PathVariable String shortLink) {
+        String originalLink = String.valueOf(linkService.findByShortLink(shortLink));
+        return (ResponseEntity<LinkEntity>) ResponseEntity.ok();
     }
 
     @DeleteMapping("/delete/{shortLink}")
-    @RequestMapping(value = "/delete/{shortLink}" , method = {RequestMethod.DELETE})
-    public ModelAndView deleteByShortLink(@NotEmpty @RequestParam(value="shortLink") String shortLink) {
+    public ResponseEntity<Void> deleteByShortLink(@PathVariable String shortLink) {
         linkService.deleteByShortLink(shortLink);
-
-        return linkList();
+        return ResponseEntity.ok().build();
     }
 }
