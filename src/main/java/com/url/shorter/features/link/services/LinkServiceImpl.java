a09f1e4cf6e9cd8e612837424e032bf7c94587f9
olexandr2009 <133679311+olexandr2009@users.noreply.github.com>
2024-01-23 20:27:44 +0200

    Update LinkServiceImpl.java

--- src/main/java/com/url/shorter/features/link/services/LinkServiceImpl.java
+++ src/main/java/com/url/shorter/features/link/services/LinkServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
