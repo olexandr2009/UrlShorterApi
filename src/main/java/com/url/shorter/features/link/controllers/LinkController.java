6b6add73c03c58fcfe693c4a4689c00e26000d5d
olexandr2009 <133679311+olexandr2009@users.noreply.github.com>
2024-01-23 20:59:29 +0200

    Revert "Link swagger" (#52)

--- src/main/java/com/url/shorter/features/link/controllers/LinkController.java
+++ src/main/java/com/url/shorter/features/link/controllers/LinkController.java
@@ -2,73 +2,40 @@ package com.url.shorter.features.link.controllers;
 
 import com.url.shorter.features.link.dto.LinkDto;
 import com.url.shorter.features.link.services.LinkService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.url.shorter.features.link.services.ShortLinkGenerator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@Tag(name = "Links", description = "Links api")
-@RequestMapping("/V1/links")
+@RequestMapping("/V1/api/links")
 public class LinkController {
 
     private final LinkService linkService;
+    private final ShortLinkGenerator shortLinkGenerator;
 
-    public LinkController(LinkService linkService) {
+    public LinkController(LinkService linkService, ShortLinkGenerator shortLinkGenerator) {
         this.linkService = linkService;
+        this.shortLinkGenerator = shortLinkGenerator;
     }
 
-    @Operation(
-            summary = "Find by short link",
-            tags = {"Links"}
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "403", description = "Unauthorized authorize in Authentication login")
-    })
     @GetMapping("/{shortLink}")
     public ResponseEntity<Optional<LinkDto>> findLinkByShortLink(@PathVariable String shortLink) {
         return ResponseEntity.ok(linkService.findByShortLink(shortLink));
     }
 
-    @Operation(
-            summary = "Delete",
-            description = "delete Link by short link",
-            tags = {"Links"}
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "403", description = "Unauthorized authorize in Authentication login")
-    })
     @DeleteMapping("/delete/{shortLink}")
     public ResponseEntity<Void> deleteByShortLink(@PathVariable String shortLink) {
         linkService.deleteByShortLink(shortLink);
         return ResponseEntity.ok().build();
     }
 
-    @Operation(
-            summary = "Save",
-            description = "save long link",
-            tags = {"Links"}
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "201"),
-            @ApiResponse(responseCode = "400", description = "Long link isn't valid"),
-            @ApiResponse(responseCode = "500"),
-            @ApiResponse(responseCode = "403", description = "Unauthorized authorize in Authentication login")
-    })
-    @PostMapping("/save")
+    @PostMapping("/saveLongLink")
     public ResponseEntity<LinkDto> saveLongLink(
             @RequestParam String longLink,
             @RequestParam(required = false) LocalDateTime dateExp,
@@ -94,16 +61,7 @@ public class LinkController {
         }
     }
 
-    @Operation(
-            summary = "Find all links",
-            tags = {"Links"}
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "500", description = "Can't get all links"),
-            @ApiResponse(responseCode = "403", description = "Unauthorized authorize in Authentication login")
-    })
-    @GetMapping("/all/find")
+    @GetMapping("/findAll")
     public ResponseEntity<List<LinkDto>> findAll() {
         try {
             List<LinkDto> allLinks = linkService.findAll();
@@ -113,33 +71,12 @@ public class LinkController {
         }
     }
 
-    @Operation(
-            summary = "Find all active links",
-            tags = {"Links"}
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200",
-                    content = @Content(
-                            schema = @Schema(implementation = Collection.class)
-                    )
-            ),
-            @ApiResponse(responseCode = "403", description = "Unauthorized authorize in Authentication login")
-    })
-    @GetMapping("/all/active")
+    @GetMapping("/active")
     public ResponseEntity<List<LinkDto>> findActiveLinks() {
         List<LinkDto> activeLinks = linkService.findActiveLinks();
         return ResponseEntity.ok(activeLinks);
     }
 
-    @Operation(
-            summary = "Delete",
-            description = "delete Link by long link",
-            tags = {"Links"}
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "403", description = "Unauthorized authorize in Authentication login")
-    })
     @DeleteMapping("/delete/{longLink}")
     public ResponseEntity<Void> deleteByLongLink(@PathVariable String longLink) {
         linkService.deleteByLongLink(longLink);
--- src/main/java/com/url/shorter/features/link/controllers/RedirectController.java
+++ src/main/java/com/url/shorter/features/link/controllers/RedirectController.java
@@ -2,39 +2,23 @@ package com.url.shorter.features.link.controllers;
 
 import com.url.shorter.features.link.dto.LinkDto;
 import com.url.shorter.features.link.services.LinkService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-@RestController
-@Tag(name = "Redirect", description = "Redirector to long Url")
+@Controller
 public class RedirectController {
     private final LinkService linkService;
 
     public RedirectController(LinkService linkService) {
         this.linkService = linkService;
     }
-    @Operation(
-            summary = "Redirect",
-            description = "redirect all users by url",
-            tags = {"Redirect"}
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "302"),
-            @ApiResponse(responseCode = "404", description = "Short link not found")
-    })
+
     @GetMapping("/{shortLink}")
-    public void redirectToLongLink(
-            @Parameter(required = true, description = "short url")
-            @PathVariable String shortLink, HttpServletResponse response) throws IOException {
+    public void redirectToLongLink(@PathVariable String shortLink, HttpServletResponse response) throws IOException {
         LinkDto linkDto = linkService.redirect(shortLink);
         if (linkDto != null) {
             response.sendRedirect(linkDto.getLongLink());
